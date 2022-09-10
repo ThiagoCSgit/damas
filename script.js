bb3fa39d10c6c1ec209a71487f1f6ad64d4a9386
ThiagoCSgit <thiago.csdossantos@gmail.com>
2022-09-10 20:04:46 -0300

    Iniciando movimentação das peças

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let board = document.querySelector('#board')
 let pieceRow = [1,2,3,6,7,8]
+let idPiece = ''
 
 function createBoard (){
     let table = document.createElement('table')
@@ -13,13 +14,17 @@ function createBoard (){
             row.appendChild(column)
             if(i % 2 == 0 && j % 2 == 0){
                 column.classList.add('white-square')
+                column.addEventListener('dragover', dropPiece)
             }
             else if(i % 2 != 0 && j % 2 != 0){
                 column.classList.add('white-square')
+                column.addEventListener('dragover', dropPiece)
             }
             if(pieceRow.includes(i)){
                 let img = document.createElement('img')
+                img.addEventListener('dragstart', startDrag)
                 if(i < 4){
+                    img.setAttribute('id', `peca_branca_${i}_${j}`)
                     if(i % 2 != 0 && j %2 != 0){
                         img.src = 'peca_branca.png'
                         column.appendChild(img)
@@ -30,6 +35,7 @@ function createBoard (){
                     }
                 }
                 else{
+                    img.setAttribute('id', `peca_preta_${i}_${j}`)
                     if(i % 2 != 0 && j %2 != 0){
                         img.src = 'peca_preta.png'
                         column.appendChild(img)
@@ -46,3 +52,17 @@ function createBoard (){
 }
 
 createBoard()
+
+function startDrag(){
+    idPiece = this.id
+}
+
+function dropPiece(){
+    let piece = document.querySelector(`#${idPiece}`)
+    if(this.querySelectorAll('img').length == 0){
+        this.appendChild(piece)
+    }
+    else{
+        console.log('Está casa já tem uma peça')
+    }
+}
