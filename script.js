14248502b896d5d0627e0fdb4d3ef2b82f5c307d
ThiagoCSgit <thiago.csdossantos@gmail.com>
2022-09-15 00:02:17 -0300

    continuando jogo de damas

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let board = document.querySelector('#board')
 let pieceRow = [1,2,3,6,7,8]
+let initialRow = ''
+let initialColumn = ''
 let idPiece = ''
 
 function createBoard (){
@@ -8,17 +10,19 @@ function createBoard (){
     table.appendChild(tbody)
     for(let i=1; i<9;i++){
         let row = document.createElement('tr')
+        row.id = `row-${i}`
         tbody.appendChild(row)
         for(let j=1; j<9;j++){
             let column = document.createElement('td')
+            column.id = `row-${i}-column-${j}`
             row.appendChild(column)
             if(i % 2 == 0 && j % 2 == 0){
                 column.classList.add('white-square')
-                column.addEventListener('dragover', dropPiece)
+                column.addEventListener('dragover', (e) => dropPiece(e))
             }
             else if(i % 2 != 0 && j % 2 != 0){
                 column.classList.add('white-square')
-                column.addEventListener('dragover', dropPiece)
+                column.addEventListener('dragover',(e) => dropPiece(e))
             }
             if(pieceRow.includes(i)){
                 let img = document.createElement('img')
@@ -55,14 +59,54 @@ createBoard()
 
 function startDrag(){
     idPiece = this.id
+    initialRow = this.parentNode.parentNode.id
+    initialColumn = this.parentNode
 }
 
-function dropPiece(){
+function dropPiece(event){
     let piece = document.querySelector(`#${idPiece}`)
-    if(this.querySelectorAll('img').length == 0){
-        this.appendChild(piece)
-    }
+    let startRow = parseInt(initialRow.split('-')[1])
+    let targetRowId = parseInt(event.path[1].id.split('-')[1])
+    let targetColumnId = parseInt(event.target.id.split('-')[3])
+    let targetColumn = document.querySelector(`#row-${targetRowId}-column-${targetColumnId}`)
+    if(piece.id.includes('preta')){
+        if(targetColumn.querySelectorAll('img').length == 0 && targetRowId == startRow - 1){
+            targetColumn.appendChild(piece)
+        }
+        else{
+            let aux = document.querySelector(`#row-${targetRowId+1}-column-${targetColumnId-1
+            }`)
+            if(aux.querySelectorAll('img').length > 0){
+                targetColumn.appendChild(piece)
+                aux.querySelector('img').style.display = 'none'
+            } else{
+                aux = document.querySelector(`#row-${targetRowId+1}-column-${targetColumnId+1}`)
+                    if(aux.querySelectorAll('img').length > 0){
+                        targetColumn.appendChild(piece)
+                        aux.querySelector('img').style.display = 'none'
+                    }
+            
+            }
+        }
+    } 
     else{
-        console.log('Está casa já tem uma peça')
+        if(targetColumn.querySelectorAll('img').length == 0 && targetRowId == startRow + 1){
+            targetColumn.appendChild(piece)
+        }
+        else{
+            let aux = document.querySelector(`#row-${targetRowId-1}-column-${targetColumnId+1
+            }`)
+            if(aux.querySelectorAll('img').length > 0){
+                targetColumn.appendChild(piece)
+                aux.querySelector('img').style.display = 'none'
+            } else{
+                aux = document.querySelector(`#row-${targetRowId+1}-column-${targetColumnId-1}`)
+                    if(aux.querySelectorAll('img').length > 0){
+                        targetColumn.appendChild(piece)
+                        aux.querySelector('img').style.display = 'none'
+                    }
+            
+            }
+        }
     }
 }
